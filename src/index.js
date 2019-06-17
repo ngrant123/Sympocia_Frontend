f3d2537424bb6f62cd4e950620bd949c33c1b181
Nathan Grant <nathangrant@Nathans-MBP-2.lan1>
2019-06-17 00:56:26 -0400

    Finished an routing issue

--- src/index.js
+++ src/index.js
@@ -4,10 +4,8 @@ import React from "react";
 import ReactDom from "react-dom";
 import Landing from "./Pages/Landing";
 import Signup from "./Components/Signup/LargeSignupComp/LSignUpPage";
-import HomeScreen from "./Components/LargeComp/LHome";
 import ProfileScreen from "./Components/Profile/LargeProfileComp/LProfile";
 import {BrowserRouter as Router, Route , Switch} from 'react-router-dom';
-import NetworkScreen from "./Components/LargeComp/LNetwork.js";
 import InvestorScreen from "./Components/Investor/LargeInvestorComp/LInvestor.js";
 
 
@@ -32,9 +30,7 @@ const application  = (
 				<Signup value = {"This is a tester"}/>
 				)
 			}/>
-			<Route path="/home" component = {HomeScreen} />
 			<Route path="/profile" component = {ProfileScreen} />
-			<Route path="/network" component = {NetworkScreen} />
 			<Route path="/investor" component= {InvestorScreen} />
 
 		</Switch>
