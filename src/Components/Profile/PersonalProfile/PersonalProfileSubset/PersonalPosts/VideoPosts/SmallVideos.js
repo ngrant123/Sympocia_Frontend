e85d288e19b99050713bbb1675b59beb0c2e5be7
Nathan Grant <nathangrant@Nathans-MBP-2.fios-router.home>
2021-01-14 06:22:45 -0500

    Fixing up video ui personal page

--- src/Components/Profile/PersonalProfile/PersonalProfileSubset/PersonalPosts/VideoPosts/SmallVideos.js
+++ src/Components/Profile/PersonalProfile/PersonalProfileSubset/PersonalPosts/VideoPosts/SmallVideos.js
@@ -145,8 +145,16 @@ const SmallVideoContainer=(videoData)=>{
 								</li>
 
 								<div id="postInformation">
-									<li style={{listStyle:"none",fontSize:"15px"}}>
-										<b>{videoData.video.title} </b>
+									<li style={{listStyle:"none",fontSize:"15px",maxWidth:"60%",maxHeight:"50px",overflow:"hidden"}}>
+										Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+										incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
+										exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+										dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+										Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit 
+										anim id est laborum.
+										{/*
+											<b>{videoData.video.title} </b>
+										*/}
 									</li>
 
 									<li style={{listStyle:"none"}}>
