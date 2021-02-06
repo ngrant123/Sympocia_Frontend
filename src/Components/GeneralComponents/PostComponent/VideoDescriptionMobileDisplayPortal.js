9c5ffd27b1f4574a17b9a6ddc7d885f18179c4d4
Nathan Grant <nathangrant@Nathans-MBP-2.fios-router.home>
2021-02-05 22:48:15 -0500

    Fixing stuff

--- src/Components/GeneralComponents/PostComponent/VideoDescriptionMobileDisplayPortal.js
+++ src/Components/GeneralComponents/PostComponent/VideoDescriptionMobileDisplayPortal.js
@@ -39,10 +39,23 @@ const VideoDescriptionMobileDisplayPortal=({videoUrl,targetDom,closeModal})=>{
 				onClick={()=>closeModal()}
 			/>
 			<Container>
-
+				<div onClick={()=>closePostModal()} style={{marginBottom:"5%"}}>
+					<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-circle-x"
+					 width="44" height="44" viewBox="0 0 24 24" stroke-width="1" stroke="#9e9e9e" fill="none" 
+					 stroke-linecap="round" stroke-linejoin="round">
+					  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+					  <circle cx="12" cy="12" r="9" />
+					  <path d="M10 10l4 4m0 -4l-4 4" />
+					</svg>
+				</div>
+				<hr/>
+				<video id="videoDescription" onClick={()=>displayVideoDescriptionTrigger()} 
+					style={{borderRadius:"50%"}} width="50%" height="50%" autoplay="true">
+					<source src={videoUrl} type="video/mp4"/>
+				</video>
 			</Container>
 		</React.Fragment>
-	,targetDom)
+	,document.getElementById(targetDom))
 }
 
 export default VideoDescriptionMobileDisplayPortal;
