e1ae9f9d9bedfe01ba381a94cfec621612eb4a4f
Nathan BC Grant <ngrant@Nathans-MBP.fios-router.home>
2021-02-10 11:35:10 -0500

    Adding mobile chrome ios

--- src/Actions/Tasks/VerifyBrowserIsNotSafari.js
+++ src/Actions/Tasks/VerifyBrowserIsNotSafari.js
@@ -1,8 +1,27 @@
 const VerifyBrowserIsChrome=()=>{
 	debugger;
-    var isChrome = navigator.vendor && navigator.vendor.indexOf('Google Inc')!=-1;
-    console.log(navigator.vendor);
-   return isChrome;
+   // var isChrome = navigator.vendor && navigator.vendor.indexOf('Google Inc')!=-1;
+  //  console.log(navigator.vendor);
+    var isChromium = window.chrome;
+	var winNav = window.navigator;
+	var vendorName = winNav.vendor;
+	var isOpera = typeof window.opr !== "undefined";
+	var isIEedge = winNav.userAgent.indexOf("Edge") > -1;
+	var isIOSChrome = winNav.userAgent.match("CriOS");
+
+	if (isIOSChrome) {
+	  return true;
+	} else if(
+	  isChromium !== null &&
+	  typeof isChromium !== "undefined" &&
+	  vendorName === "Google Inc." &&
+	  isOpera === false &&
+	  isIEedge === false
+	) {
+	  return true;
+	} else { 
+	   return false;
+	}
 }
 
 
