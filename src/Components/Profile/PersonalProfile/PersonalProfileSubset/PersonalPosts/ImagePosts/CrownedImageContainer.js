bc3bf57b5a2349b8baf6c3ba7f6d0dbab0695292
Nathan BC Grant <ngrant@Nathans-MBP.fios-router.home>
2021-02-23 14:22:50 -0500

    Fixing stuff

--- src/Components/Profile/PersonalProfile/PersonalProfileSubset/PersonalPosts/ImagePosts/CrownedImageContainer.js
+++ src/Components/Profile/PersonalProfile/PersonalProfileSubset/PersonalPosts/ImagePosts/CrownedImageContainer.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 
+
 const Container=styled.div`
 	position:relative;
 	z-index:25;
@@ -12,19 +13,9 @@ const Container=styled.div`
     		width:30% !important;
     		margin-left:-5%;
     	}
-    }
-
-    @media screen and (max-width:420px){
-    	width:160% !important;
     	#postInformationLI{
     		display:none !important;
     	}
-	}
-
-	@media screen and (max-width:340px){
-		#crownedImageContainer{
-			width:210px !important;
-		}
     }
 
     @media screen and (max-width:1370px) and (max-height:1030px) and (orientation: landscape) {
@@ -35,47 +26,33 @@ const Container=styled.div`
 
 
     @media screen and (max-width:840px) and (max-height:420px) and (orientation: landscape){
-	 	#crownedImageContainer{
-			width:180px !important;
-		}
+	 	
 		#parentContainer{
 			height:100% !important;
-		}
-    }
-    @media screen and (max-width:670px) and (max-height:380px){
-	 	#crownedImageContainer{
-			width:160px !important;
-		}
-		#postInformationLI{
-			display:none !important;
-		}
-		#parentContainer{
-			margin-left:2% !important;
 		}
     }
 `;
 
 const Image=styled.div`
 	position:relative;
-	width:100%;
+	width:30%;
 	height:90%;
 	background-color:blue;
 	border-radius:5px;
 	overflow:hidden;
 
-	@media screen and (max-width:770px){
-		width:90% !important;
-    }
-	
-
-	@media screen and (max-width:340px){
-		width:90% !important;
+	@media screen and (max-width:1370px){
+		width:60% !important;
+	}
+	@media screen and (max-width:650px){
+		width:80% !important;
     }
 
-     @media screen and (max-width:740px) and (max-height:420px){
-	 	width:80% !important;
+     @media screen and (max-width:840px) and (max-height:420px) and (orientation: landscape){
+	 	width:70% !important;
     }
 `;
+
 
 const Description=styled.div`
 	position:relative;
@@ -123,21 +100,19 @@ const CrownedImageContainer=(props)=>{
 
 	return(
 		<Container>
-			<ul id="parentContainer" style={{padding:"0px",height:"45%",overflow:"hidden"}}>
-				<li style={{listStyle:"none",display:"inline-block",marginRight:"1%"}}>
-					<Image>
-						<img src={imageData.imgUrl} style={{width:"100%",height:"100%"}}/>
-						<VideoDesriptionContainer>
-							<video key={videoDescriptionId} autoPlay loop autoBuffer muted playsInline 
-								style={{borderRadius:"50%"}} width="100%" height="100%" borderRadius="50%">
-								<source src={imageData.videoDescription} type="video/mp4"/>
-							</video>
+			<div id="parentContainer" style={{display:"flex",flexDirection:"row",padding:"0px",height:"45%",overflow:"hidden"}}>
+				<Image>
+					<img src={imageData.imgUrl} style={{width:"100%",height:"100%"}}/>
+					<VideoDesriptionContainer>
+						<video key={videoDescriptionId} autoPlay loop autoBuffer muted playsInline 
+							style={{borderRadius:"50%"}} width="100%" height="100%" borderRadius="50%">
+							<source src={imageData.videoDescription} type="video/mp4"/>
+						</video>
 
-						</VideoDesriptionContainer>
-					</Image>
-				</li>
+					</VideoDesriptionContainer>
+				</Image>
 
-				<li id="postInformationLI" style={{position:"absolute",top:"0%",listStyle:"none",display:"inline-block",width:"50%"}}>
+				<div id="postInformationLI" style={{top:"0%",width:"50%"}}>
 					<ul style={{paddging:"0px"}}>
 						<li style={IndustryButtonCSS}>
 							{imageData.industriesUploaded[0].industry}
@@ -184,8 +159,8 @@ const CrownedImageContainer=(props)=>{
 							</ul>
 						</li>
 					</ul>
-				</li>
-			</ul>
+				</div>
+			</div>
 		</Container>
 	)
 }
