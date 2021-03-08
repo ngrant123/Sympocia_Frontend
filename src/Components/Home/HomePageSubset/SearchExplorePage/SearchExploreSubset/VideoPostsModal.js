13d1cb9474a94b1acafea883d7e0e76ccce7b891
Nathan BC Grant <ngrant@Nathans-MBP.fios-router.home>
2021-03-08 09:49:58 -0500

    Redesigning video ui

--- src/Components/Home/HomePageSubset/SearchExplorePage/SearchExploreSubset/VideoPostsModal.js
+++ src/Components/Home/HomePageSubset/SearchExplorePage/SearchExploreSubset/VideoPostsModal.js
@@ -17,15 +17,6 @@ const Container=styled.div`
 	display:flex;
 	top:5%;
 	flex-direction:row;
-	@media screen and (max-width:740/px) and (max-height:420px){
-    	#headerLI{
-			height:180% !important;
-		}
-		#headerVideoLI{
-			height:95% !important;
-		}
-    }
-
 
 	@media screen and (max-width:1370px){
 		width:100%;
@@ -36,35 +27,30 @@ const Container=styled.div`
 			display:none !important;
 		}
 		#headerVideoLI{
-			height:80% !important;
-			width:80% !important;
+			height:600px !important;
+			width:650px !important;
 		}
 		#smallPostLI{
 			width:95% !important;
 			margin-left:-5% !important;
 		}
-		#video{
-			width:330px !important;
-			height:370px !important;
-	
-			margin-right:2%;
+		#horizontalSeperator{
+			display:block !important;
 		}
 	}
 
-	@media screen and (max-width:740px){
-		#ownerFirstName{
-			display:none !important;
-		}
+	@media screen and (max-width:650px){
 		#headerAudio{
 			height:20px !important;
+		}
+		
+		#headerVideoLI{
+			height:200px !important;
+			width:250px !important;
 		}
 	}
 	@media screen and (max-width:450px){
 		margin-left:-5% !important;
-		#video{
-			width:150px !important;
-			height:auto !important;
-		}
 	}
 `;
 
@@ -111,7 +97,6 @@ const ShadowContainer= styled.div`
 
 const ProfilePictureLink=styled(Link)`
 	position:relative;
-	overflow:hidden;
 `;
 
 
@@ -136,39 +121,64 @@ const HeaderContainer=styled.div`
 		#headerPostProfilePictureLIInformation{
 			top:60% !important;
 		}
+		#videoDescriptionContainer{
+			top:25% !important;
+			left:0% !important;
+		}
 	}
 
-	@media screen and (max-width:600px){
+
+	@media screen and (max-width:650px){
 		margin-top:-130px !important;
 		#headerPostProfilePictureLIInformation{
 			top:-30% !important;
 		}
+		#videoDescriptionContainer{
+			top:25% !important;
+			left:0% !important;
+			width:100px !important;
+			height:40% !important;
+		}
 	}
 
-	@media screen and (max-width:1370px) and (max-height:900px) and (orientation: landscape) {
-		#headerPostProfilePictureLIInformation{
-			top:110% !important;
-		}
-    }
 
-
-	@media screen and (max-width:740px) and (max-height:420px) and (orientation: landscape) {
+	@media screen and (max-width:840px) and (max-height:420px) and (orientation: landscape) {
     	margin-top:60px !important;
     	#headerPostProfilePictureLIInformation{
 			top:160% !important;
+		}
+		#headerVideoLI{
+			height:400px !important;
+			width:450px !important;
+		}
+		#videoDescriptionContainer{
+			top:20% !important;
+		}
+		#headerVideoContainer{
+			position:relative !important;
 		}
     }
 `;
 
 const HeaderDescriptionContainer=styled.div`
 	display:flex;
-	flex-direction:row;
+	flex-direction:column;
 	margin-top:1%;
+
+	@media screen and (max-width:650px){
+		#headerDescriptionParagraph{
+			max-width:100% !important;
+		}
+		#heaerCaptionParagraph{
+			width:100% !important;
+		}
+	}
 `;
 
 const HeaderTextsContainer=styled.div`
 	display:flex;
 	flex-direction:column;
+	margin-left:2%;
 `;
 
 const SmallPostContainer=styled.div`
@@ -183,23 +193,62 @@ const SmallPostContainer=styled.div`
 		width:100%;
 	}
 
+	@media screen and (max-width:650px){
+		margin-top:10%;
+		margin-left:0%;
+
+		#video{
+			width:250px !important;
+			height:150px !important;
+		}
+	}
+
 `;
 
 const HeaderOwnerInformation=styled.div`
 	display:flex;
 	flex-direction:row;
+
+	@media screen and (max-width:1370px){
+		#ownerInformationDiv{
+			width:100% !important;
+		}
+	}
+	@media screen and (max-width:650px){
+		flex-direction:column;
+		#headerOwnerName{
+			font-size:15px !important;
+			max-width:90% !important;
+		}
+	}
 `;
 
 const DescriptionContainer=styled.div`
 	display:flex;
 	flex-direction:row;
-
+	width:200%;
 `;
 
 const PostContainer=styled.div`
 	margin-bottom:8%;
 	width:35%;
 	margin-right:8%;
+
+	@media screen and (max-width:1370px){
+		#smallVideoDescriptionContainer{
+			margin-left:5%;
+		}
+	}
+
+	@media screen and (max-width:650px){
+		#smallVideoDescriptionContainer{
+			width:100px !important;
+			height:40% !important;
+			margin-left:5%;
+			top:25% !important;
+			left:0% !important;
+		}
+	}
 `;
 
 const ImageLabelCSS={
@@ -229,6 +278,12 @@ const NextButtonCSS={
 	padding:"10px",
 	borderRadius:"5px",
 	cursor:"pointer"
+}
+
+const HorizontalLineCSS={
+	marginLeft:"0",
+	marginRight:"0",
+	display:"none"
 }
 
 const VideoPostModal=(props)=>{
@@ -269,129 +324,90 @@ const VideoPostModal=(props)=>{
 				<React.Fragment>
 					<HeaderContainer>
 						<HeaderOwnerInformation>
-							<li style={{listStyle:"none",display:"inline-block"}}>
+							<div id="ownerInformationDiv" style={{display:"flex",flexDirection:"row",width:"60%"}}>
 								<ProfilePictureLink to={{pathname:`/profile/${headerVideo.owner._id}`}}>
-									{headerVideo.videoDescription==null?
-										<img src={headerVideo.owner.profilePicture==null?NoProfilePicture:
-											headerVideo.owner.profilePicture}
-											style={{height:"50px",width:"60px",borderRadius:"50%"}}
-										/>
-										:<video style={{borderRadius:"50%"}} width="50px" height="60px" borderRadius="50%" autoplay="true" muted>
-											<source src={headerVideo.videoDescription} type="video/mp4"/>
-										</video>
-									}
-								</ProfilePictureLink>
-							</li>
-
-								<p id="ownerFirstName" 
-									style={{fontSize:"20px",maxWidth:"60%",maxHeight:"50px",overflow:"hidden",marginRight:"5%"}}>
-									{headerVideo.owner.firstName}
-								</p>
-
-							{props.isGuestProfileIndicator==false &&(
-								<li style={{marginLeft:"1%",marginBottom:"1%",listStyle:"none"}}>
-									<DisplayRecruitButton
-										post={headerVideo}
-										previousProps={props}
-										personalInformationRedux={personalInformationRedux}
+									<img src={headerVideo.owner.profilePicture==null?NoProfilePicture:
+										headerVideo.owner.profilePicture}
+										style={{height:"50px",width:"60px",borderRadius:"50%"}}
 									/>
-								</li>
-							)}
+								</ProfilePictureLink>
+								<Link id="headerOwnerName" to={{pathname:`/profile/${headerVideo.owner._id}`}}
+									style={{fontSize:"20px",maxWidth:"60%",maxHeight:"50px",overflow:"hidden",marginRight:"5%"}}>
+									<b>{headerVideo.owner.firstName}</b>
+								</Link>
+							</div>
 							{headerVideo.audioDescription!=null &&(
-								<li style={{listStyle:"none",display:"inline-block"}}>
-									<audio id="headerAudio" style={{width:"200px"}} controls>
-									  	<source src={headerVideo.audioDescription} type="audio/ogg"/>
-									  	<source src={headerVideo.audioDescription} type="audio/mp4"/>
-										Your browser does not support the audio element.
-									</audio>
-								</li>
+								<audio id="headerAudio" style={{width:"200px",marginTop:"2%"}} controls>
+								  	<source src={headerVideo.audioDescription} type="audio/ogg"/>
+								  	<source src={headerVideo.audioDescription} type="audio/mp4"/>
+									Your browser does not support the audio element.
+								</audio>
 							)}
 						</HeaderOwnerInformation>
+						<div id="headerVideoLI" style={{height:"100%",width:"90%",position:"relative"}}>
+							<video id="headerVideoContainer" style={{cursor:"pointer",position:"absolute"}} height="100%" width="90%" borderRadius="50%"
+							 key={headerVideo.videoUrl} autoPlay loop autoBuffer muted playsInline onClick={()=>handleDisplayHeaderVideo()}>
+								<source src={headerVideo.videoUrl} type="video/mp4"/>
+							</video>
+							<video id="videoDescriptionContainer" autoPlay loop autoBuffer muted playsInline 
+								style={{position:"absolute",top:"50px",left:"0%"}} width="200px" height="60%">
+								<source src={headerVideo.videoDescription} type="video/mp4"/>
+							</video>
+						</div>
 
-						<video id="headerVideoLI" style={{cursor:"pointer"}} width="90%" height="100%" borderRadius="50%"
-						 key={headerVideo.videoUrl} autoPlay loop autoBuffer muted playsInline onClick={()=>handleDisplayHeaderVideo()}>
-							<source src={headerVideo.videoUrl} type="video/mp4"/>
-						</video>
 						<HeaderDescriptionContainer>
-							<HeaderTextsContainer>
-								<p style={{fontSize:"20px",maxWidth:"70%",maxHeight:"60px",overflow:"hidden"}}>
-									<b>
-										{headerVideo.title}
-									</b>
-								</p>
-								<p style={{width:"70%",maxHeight:"60px",overflow:"hidden"}}>
-									{headerVideo.description}
-								</p>
-							</HeaderTextsContainer>
-							{/*
-								<p id="symposiumText" style={ImageLabelCSS}>
-									{headerVideo.industriesUploaded[0].industry}
-								</p>
-							*/}
+							<p id="headerDescriptionParagraph" style={{fontSize:"20px",maxWidth:"70%",maxHeight:"60px",overflow:"hidden"}}>
+								<b>
+									{headerVideo.title}
+								</b>
+							</p>
+							<p id="heaerCaptionParagraph" style={{width:"70%",maxHeight:"60px",overflow:"hidden"}}>
+								{headerVideo.description}
+							</p>
 						</HeaderDescriptionContainer>
-
-
 					</HeaderContainer>
+
+					<hr id="horizontalSeperator" style={HorizontalLineCSS}/>
+
 					<SmallPostContainer>
 						{videos.map(data=>
 							<React.Fragment>
-								{/*
-									For some reason the css of the suggested symposiums is messed up on production so just showing 
-									videos on here.
-									{data.owner==null?
-										<ConstructSuggestedSymposium
-											personalInformation={personalInformationRedux}
-											previousProps={props}
-										/>
-								*/}
 								{data.owner!=null &&(
 									<PostContainer>
-										<video onClick={()=>displayVideoModal(data)} style={{listStyle:"none",cursor:"pointer"}}
-											id="video" position="relative" height="290px" width="160%" borderRadius="50%"
-										 	key={data.videoUrl} autoPlay loop autoBuffer muted playsInline>
-											<source src={data.videoUrl} type="video/mp4"/>
-										</video>
-										<ul style={{padding:"0px",zIndex:"8"}}>
-											<li style={{listStyle:"none",display:"inline-block",marginRight:"5%"}}>
-												<ProfilePictureLink to={{pathname:`/profile/${data.owner._id}`}}>
-													{data.videoDescription==null?
-														<img src={data.owner.profilePicture==null?NoProfilePicture:data.owner.profilePicture}
-															 style={{height:"50px",width:"60px",borderRadius:"50%"}}
-														/>
-														:<video style={{borderRadius:"50%"}} width="60px" height="50px" borderRadius="50%" autoplay="true" controls muted>
-															<source src={data.videoDescription} type="video/mp4"/>
-														</video>
-													}
-												</ProfilePictureLink>
+										{data.audioDescription!=null &&(
+											<li id="smallAudioDescription" style={{listStyle:"none"}}>
+												<audio style={{width:"150px",height:"25px"}} controls muted>
+												  	<source src={data.audioDescription} type="audio/ogg"/>
+												  	<source src={data.audioDescription} type="audio/mp4"/>
+													Your browser does not support the audio element.
+												</audio>
 											</li>
-											{props.isGuestProfileIndicator==false &&(
-												<li style={{listStyle:"none",display:"inline-block"}}>
-													<DisplayRecruitButton
-														post={data}
-														previousProps={props}
-														personalInformationRedux={personalInformationRedux}
-													/>
-												</li>
-											)}
-											{data.audioDescription!=null &&(
-												<li id="smallAudioDescription" style={{listStyle:"none"}}>
-													<audio style={{width:"150px",height:"25px"}} controls muted>
-													  	<source src={data.audioDescription} type="audio/ogg"/>
-													  	<source src={data.audioDescription} type="audio/mp4"/>
-														Your browser does not support the audio element.
-													</audio>
-												</li>
-											)}
-										</ul>
+										)}
+										<div id="video" style={{height:"280px",width:"200%",position:"relative"}}>
+											<video onClick={()=>displayVideoModal(data)} style={{backgroundColor:"#151515",position:"absolute",cursor:"pointer"}}
+												 position="relative" height="90%" width="100%" borderRadius="50%"
+											 	key={data.videoUrl} autoPlay loop autoBuffer muted playsInline>
+												<source src={data.videoUrl} type="video/mp4"/>
+											</video>
+											<video id="smallVideoDescriptionContainer" autoPlay loop autoBuffer muted playsInline 
+												style={{position:"absolute",top:"50%",left:"5%"}} width="100px" height="40%">
+												<source src={data.videoDescription} type="video/mp4"/>
+											</video>
+										</div>
 										<DescriptionContainer>
+											<ProfilePictureLink to={{pathname:`/profile/${data.owner._id}`}}>
+												<img src={data.owner.profilePicture==null?NoProfilePicture:data.owner.profilePicture}
+													 style={{height:"50px",width:"60px",borderRadius:"50%"}}
+												/>
+											</ProfilePictureLink>
 											<HeaderTextsContainer>
 												<p style={{fontSize:"20px",maxWidth:"100%",maxHeight:"60px",overflow:"hidden"}}>
 													<b>
 														{data.title}
 													</b>
 												</p>
-												<p style={{width:"70%",maxWidth:"100%",height:"60px",overflow:"hidden"}}>
-													{data.description}
+												<p style={{fontSize:"15px",width:"70%",maxWidth:"100%",height:"60px",overflow:"hidden"}}>
+													{data.owner.firstName}
 												</p>
 											</HeaderTextsContainer>
 										</DescriptionContainer>
@@ -410,7 +426,6 @@ const VideoPostModal=(props)=>{
 							</React.Fragment>
 						)}
 					</SmallPostContainer>
-
 				{displayVideoDisplayPortal==false?
 					null:
 					<VideoPostDisplayPortal
