6717a56ed00835b887b61d0be4313b9b5c21a3bc
Nathan BC Grant <ngrant@Nathans-MBP.fios-router.home>
2021-03-09 08:50:09 -0500

    Working on redesiging image post display modal

--- src/Components/GeneralComponents/PostComponent/ImageComponent/ImageDisplay/ImageContainer.js
+++ src/Components/GeneralComponents/PostComponent/ImageComponent/ImageDisplay/ImageContainer.js
@@ -29,7 +29,8 @@ import {
 	CrownPostModal,
 	CommentContainer,
 	PersonalInformation,
-	Post
+	Post,
+	PollingOptionsContainer
 } from "./ImageContainerCSS.js";
 import MobileUI from "./MobileUI.js";
 import DeletePostConfirmationPortal from "../../../../Profile/PersonalProfile/PersonalProfileSet/Modals-Portals/DeletePostConfirmationPortal.js";
@@ -43,6 +44,8 @@ import LoyaltyIcon from '@material-ui/icons/Loyalty';
 import BorderColorIcon from '@material-ui/icons/BorderColor';
 import AssessmentIcon from '@material-ui/icons/Assessment';
 import ChatIcon from '@material-ui/icons/Chat';
+import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
+import PollOptionPortal from "../../PollOptionPortal.js";
 
 const ButtonCSS={
   listStyle:"none",
@@ -54,7 +57,8 @@ const ButtonCSS={
   borderStyle:"solid",
   borderWidth:"2px",
   borderColor:"#3898ec",
-  marginRight:"4%"
+  marginRight:"4%",
+  cursor:"pointer"
 }
 
 const ShadowButtonCSS={
@@ -67,7 +71,15 @@ const ShadowButtonCSS={
 	borderRadius:"50%",
 	borderStyle:"none",
 	marginRight:"5%",
-	marginBottom:"2%"
+	marginBottom:"2%",
+	cursor:"pointer"
+}
+
+const PollingOptionsCSS={
+	boxShadow:"1px 1px 5px #6e6e6e",
+	padding:"10px",
+	borderRadius:"5px",
+	cursor:"pointer"
 }
 
 
@@ -79,7 +91,7 @@ const ShadowButtonCSS={
 
 const ImageContainer=(props)=>{
 	
-	const [commentImageIndicator,changeIndicator]=useState(true);
+	const [commentImageIndicator,changeCommentsDisplay]=useState(false);
 	const [displayImageModal,changeDisplayImage]=useState(false);
 	const [displayStampEffect,changeDisplayStampEffect]=useState(false);
 	const [displayMobileUI,changeUIStatus]=useState(false);
@@ -94,6 +106,9 @@ const ImageContainer=(props)=>{
 	const dispatch=useDispatch();
 	const [isLoading,changeIsLoadingStatus]=useState(true);
 	const [postData,changePostData]=useState(props);
+	const [displayPollingOptions,changeDisplayPollingOptions]=useState(false);
+	const [displayPollModal,changeDisplayPollingModal]=useState(false);
+	const [displayApproveModal,changeDisplayApproveModal]=useState(false);
 
 	useEffect(()=>{
 		const fetchData=async()=>{
@@ -201,7 +216,7 @@ const ImageContainer=(props)=>{
 	}
 
 	const hideComments=()=>{
-		changeIndicator(true);
+		changeCommentsDisplay(false);
 	}
 
 	const triggerPromoteModal=()=>{
@@ -217,11 +232,90 @@ const ImageContainer=(props)=>{
 		changeDisplayDeleteConfirmation(false);
 	}
 
+	const displayComments=()=>{
+		changeCommentsDisplay(true)
+	}
+
+	const userActionsContainer=()=>{
+		return(
+			<React.Fragment>
+				<LoyaltyIcon
+					style={{fontSize:50,...ShadowButtonCSS}}
+					onClick={()=>createOrRemoveStampEffect({isAccessTokenUpdated:false})}
+				/>
+				<ChatIcon
+					style={{fontSize:50,...ShadowButtonCSS}}
+					onClick={()=>displayComments()}
+				/>
+
+				<AssessmentIcon
+					style={{fontSize:50,...ShadowButtonCSS}}
+					onClick={()=>changeDisplayPollingOptions(true)}
+				/>
+					
+				{(postData.profileType=="personalProfile" && postData.isOwnProfile==true) &&(
+					<>
+						<BorderColorIcon
+							style={{fontSize:50,...ShadowButtonCSS}}
+							onClick={()=>changeDisplayImage(!displayImageModal)}
+						/>
+
+						<svg id="removePostOption" onClick={()=>handleRemoveImagePost()}
+							 xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-trash"
+							width="300" height="50" viewBox="0 0 24 24" stroke-width="1.5" stroke="#6e6e6e" fill="none"
+							stroke-linecap="round" stroke-linejoin="round" style={ShadowButtonCSS}>
+						  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+						  <line x1="4" y1="7" x2="20" y2="7" />
+						  <line x1="10" y1="11" x2="10" y2="17" />
+						  <line x1="14" y1="11" x2="14" y2="17" />
+						  <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" />
+						  <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" />
+						</svg>
+						<svg id="promotePostOption" onClick={()=>triggerPromoteModal()}
+							xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-award" 
+							  width="300" height="50" viewBox="0 0 24 24" stroke-width="1.5" stroke="#6e6e6e"
+							  fill="none" stroke-linecap="round" stroke-linejoin="round" style={ShadowButtonCSS}>
+							  <path stroke="none" d="M0 0h24v24H0z"/>
+							  <circle cx="12" cy="9" r="6" />
+							  <polyline points="9 14.2 9 21 12 19 15 21 15 14.2" transform="rotate(-30 12 9)" />
+							  <polyline points="9 14.2 9 21 12 19 15 21 15 14.2" transform="rotate(30 12 9)" />
+						</svg>
+					</>
+				)}
+			</React.Fragment>
+		)
+	}
+
+	const postInformationAdditionalInformation=()=>{
+		return(
+			<React.Fragment>
+				<p style={{fontSize:"20px",listStyle:"none",height:"60px",overflowY:"hidden",marginBottom:"2%"}}>
+					<b>
+						{postData.imageData.caption}
+					</b>
+				</p>
+
+				<p style={{fontSize:"13px",color:"#8c8c8c",listStyle:"none",height:"50px",overflowY:"hidden"}}>
+					{postData.imageData.description}
+				</p>
+			</React.Fragment>
+		)
+	}
+
+	const closePollingModal=()=>{
+		changeDisplayPollingModal(false);
+	}
+
+	const displayPollingOptionsTrigger=(indicator)=>{
+		changeDisplayPollingModal(true);
+		changeDisplayApproveModal(indicator);
+	}
+
 
 	return(
 		<ImageProvider value={{
 			updateIndicator:(indicator)=>{
-				changeIndicator(indicator);
+				changeCommentsDisplay(indicator);
 			}
 		}}>
 			<React.Fragment>
@@ -239,6 +333,16 @@ const ImageContainer=(props)=>{
 						targetDom={"personalContainer"}
 					/>
 				)}
+				{displayPollModal==true?
+					<PollOptionPortal
+						closeModal={closePollingModal}
+						displayApproveModal={displayApproveModal}
+						postId={postData.imageData._id}
+						postType="Images"
+						targetDom={"personalContainer"}
+						isGuestProfile={isGuestProfile}
+					/>:null
+				}
 				{displayImageModal==true?
 					<EditImageCreation
 						imageSrcUrl={postData.imageData.imgUrl}
@@ -249,91 +353,94 @@ const ImageContainer=(props)=>{
 						{isLoading==true?
 							<p>Gives us one second while we get this post</p>:
 							<Container>
-								<PersonalInformation>
-									<img id="ownerProfilePicture" 
-										src={postData.imageData.owner.profilePicture==null?
-										NoProfilePicture:postData.imageData.owner.profilePicture}
-									 style={{borderRadius:"50%",width:"7%",height:"60px"}}
-									/>
-									<Link style={{marginLeft:"4%",fontSize:"20px",maxWidth:"80%",maxHeight:"30px",overflow:"hidden",textDecoration:"none",color:"black",marginRight:"10%"}}
-										to={{pathname:`/profile/${postData.imageData.owner._id}`}}
-									>	
-										<p>
-											<b>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliq</b>
-										</p>
-									</Link>
-									<LoyaltyIcon
-										style={{fontSize:50,...ShadowButtonCSS}}
-									/>
-									<ChatIcon
-										style={{fontSize:50,...ShadowButtonCSS}}
-									/>
-
-									<AssessmentIcon
-										style={{fontSize:50,...ShadowButtonCSS}}
-									/>
-										
-									{(postData.profileType=="personalProfile" && postData.isOwnProfile==true) &&(
-										<>
-											<BorderColorIcon
-												style={{fontSize:50,...ShadowButtonCSS}}
+								{(commentImageIndicator==false && displayPollingOptions==false)==true?
+									<React.Fragment>
+										<PersonalInformation>
+											<img id="ownerProfilePicture" 
+												src={postData.imageData.owner.profilePicture==null?
+												NoProfilePicture:postData.imageData.owner.profilePicture}
+											 style={{borderRadius:"50%",width:"7%",height:"60px"}}
 											/>
-
-											<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-trash"
-												width="30" height="30" viewBox="0 0 24 24" stroke-width="1.5" stroke="#1C1C1C" fill="none"
-												stroke-linecap="round" stroke-linejoin="round" style={ShadowButtonCSS}>
-											  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-											  <line x1="4" y1="7" x2="20" y2="7" />
-											  <line x1="10" y1="11" x2="10" y2="17" />
-											  <line x1="14" y1="11" x2="14" y2="17" />
-											  <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" />
-											  <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" />
-											</svg>
-											<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-award" 
-												  width="30" height="30" viewBox="0 0 24 24" stroke-width="1.5" stroke="#1C1C1C"
-												  fill="none" stroke-linecap="round" stroke-linejoin="round" style={ShadowButtonCSS}>
-												  <path stroke="none" d="M0 0h24v24H0z"/>
-												  <circle cx="12" cy="9" r="6" />
-												  <polyline points="9 14.2 9 21 12 19 15 21 15 14.2" transform="rotate(-30 12 9)" />
-												  <polyline points="9 14.2 9 21 12 19 15 21 15 14.2" transform="rotate(30 12 9)" />
-											</svg>
-										</>
-									)}
-								</PersonalInformation>
-								<Post>
-									{postData.imageData.videoDescription==null?null:
-										<VideoDesriptionContainer>
-											<video id="videoDescription"
-												width="100%" height="100%" borderRadius="50%"
-												autoPlay loop autoBuffer muted playsInline controls>
-												<source src={postData.imageData.videoDescription} type="video/mp4"/>
-											</video>
-										</VideoDesriptionContainer>
-									}
-									<Image>	
-										{displayStampEffect==true?
+											<Link style={{marginLeft:"4%",fontSize:"20px",maxWidth:"80%",maxHeight:"30px",overflow:"hidden",textDecoration:"none",color:"black",marginRight:"10%"}}
+												to={{pathname:`/profile/${postData.imageData.owner._id}`}}
+											>	
+												<p>
+													<b>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliq</b>
+												</p>
+											</Link>
+											{displayMobileUI==false ?
 												<React.Fragment>
-													<StampIconEffect
-														id="stampEffect"
-													>
-														<img src={StampIcon} style={{width:"100%",height:"100%",borderRadius:"50%"}}/>
-													</StampIconEffect>
+													{userActionsContainer()}
 												</React.Fragment>:
-										null}
-										<img src={postData.imageData.imgUrl} style={{width:"100%",height:"100%",borderRadius:"5px"}}/>
-									</Image>
-								</Post>
-								<div>
-									<p style={{fontSize:"20px",listStyle:"none",height:"60px",overflowY:"hidden",marginBottom:"2%"}}>
-										<b>
-											{postData.imageData.caption}
-										</b>
-									</p>
+												<KeyboardArrowDownIcon
+													id="keyBoardDownLI"
+													style={{borderRadius:"50%",fontSize:"40",boxShadow:"1px 1px 5px #dbdddf"}}
+												/>
+											}
+										</PersonalInformation>
 
-									<p style={{fontSize:"13px",color:"#8c8c8c",listStyle:"none",height:"50px",overflowY:"hidden"}}>
-										{postData.imageData.description}
-									</p>
-								</div>
+										<hr/>
+										<div style={{marginLeft:"10%",marginBottom:"2%"}}>
+											<audio id="audio" style={{width:"800px"}} controls>
+												<source src={postData.imageData.audioDescription} type="audio/ogg"/>
+												<source src={postData.imageData.audioDescription} type="audio/mp4"/>
+												Your browser does not support the audio element.
+											</audio>
+										</div>
+										<Post>
+											{postData.imageData.videoDescription==null?null:
+												<VideoDesriptionContainer>
+													<video id="videoDescription"
+														width="100%" height="100%" borderRadius="50%"
+														autoPlay loop autoBuffer muted playsInline controls>
+														<source src={postData.imageData.videoDescription} type="video/mp4"/>
+													</video>
+												</VideoDesriptionContainer>
+											}
+											<Image>	
+												{displayStampEffect==true?
+														<React.Fragment>
+															<StampIconEffect
+																id="stampEffect"
+															>
+																<img src={StampIcon} style={{width:"100%",height:"100%",borderRadius:"50%"}}/>
+															</StampIconEffect>
+														</React.Fragment>:
+												null}
+												<img id="image" src={postData.imageData.imgUrl} style={{width:"100%",height:"100%",borderRadius:"5px"}}/>
+											</Image>
+										</Post>
+										{displayMobileUI==true ?
+											<div style={{display:"flex",flexDirection:"row",flexWrap:"wrap"}}>
+												{userActionsContainer()}
+											</div>:
+											<div id="postInformation">
+												{postInformationAdditionalInformation()}
+											</div>
+										}
+									</React.Fragment>:
+									<CommentContainer>
+										{displayPollingOptions==false?
+											<Comments
+												postId={postData.imageData._id}
+												postType={"Images"}
+												hideComments={hideComments}
+												targetDom={postData.targetDom}
+												isGuestProfile={isGuestProfile}
+											/>:
+											<PollingOptionsContainer>
+												<p onClick={()=>changeDisplayPollingOptions(false)} style={{marginBottom:"10%",...ButtonCSS}}>Back</p>
+												<p onClick={()=>displayPollingOptionsTrigger(true)} style={PollingOptionsCSS}>
+													Approve Post
+												</p>
+
+												<p onClick={()=>displayPollingOptionsTrigger(false)} style={PollingOptionsCSS}>
+													Disapprove Post
+												</p>
+											</PollingOptionsContainer>
+										}
+									</CommentContainer>
+								}
 							</Container>
 						}
 					</React.Fragment>
