73285500e8483b96fe329a357308fddad2d74d27
Nathan Grant <nathangrant@Nathans-MBP-2.fios-router.home>
2021-11-25 10:59:21 -0500

    Starting airplanes creation

---
+++ src/Components/GeneralComponents/Aireplanes/index.js
@@ -0,0 +1,16 @@
+import React,{useEffect} from "react";
+
+
+const AirplaneRender=({currentPage})=>{
+	useEffect(()=>{
+
+	},[]);
+
+	return(
+		<React.Fragment>
+		</React.Fragment>
+	)
+}
+
+
+export default AirplaneRender;
